e648d43a9cdf6e05ac0868e4fe6cd96a8c6f54aa
Alwin <github@alwin.net.au>
2020-05-16 14:14:22 +0200

    Add CORS support to access backend locally

--- app_config.py
+++ app_config.py
@@ -4,12 +4,15 @@
 import logging
 import os
 import connexion
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 import config
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = connexion.App(__name__, specification_dir=basedir)
+CORS(app.app)
+
 application = app.app
 
 application.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URI
