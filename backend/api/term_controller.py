c31fa2d28c19c0b134948f95a880681cc64e0193
Alwin <github@alwin.net.au>
2020-05-16 10:12:07 +0200

    Add pagination to terms lists, fix minimum page number

--- backend/api/term_controller.py
+++ backend/api/term_controller.py
@@ -6,22 +6,22 @@ from backend.service.term_service import get_terms_by_type
 from flask import abort
 
 
-def get_all_event_terms(text: str=""):
+def get_all_event_terms(page: int, per_page: int, text: str = ""):
     valid_text = fullmatch(r"[\p{L} ]*", text)
     if valid_text is None:
         abort(400, "Input search string invalid, only letters and spaces allowed")
-    terms = get_terms_by_type("event-categories", text=text)
+    terms = get_terms_by_type("event-categories", text=text, page=page, per_page=per_page)
     if len(terms) == 0:
         abort(404, "No terms found")
     else:
         return terms
 
 
-def get_all_group_terms(text: str=""):
+def get_all_group_terms(page: int, per_page: int, text: str = ""):
     valid_text = fullmatch(r"[\p{L} ]*", text)
     if valid_text is None:
         abort(400, "Input search string invalid, only letters and spaces allowed")
-    terms = get_terms_by_type("bp_group_type", text=text)
+    terms = get_terms_by_type("bp_group_type", text=text, page=page, per_page=per_page)
     if len(terms) == 0:
         abort(404, "No terms found")
     else:
--- backend/service/term_service.py
+++ backend/service/term_service.py
@@ -9,11 +9,11 @@ def term_to_dict(term: Term):
             "slug": term.slug}
 
 
-def get_terms_by_type(type: str, text: str):
+def get_terms_by_type(type: str, text: str, page: int, per_page:int):
     text_condition = Term.name.like("%{}%".format(text)) if text != "" else True
-    terms = Term.query.filter(Term.type == type, text_condition).all()
+    terms = Term.query.filter(Term.type == type, text_condition).order_by(Term.name.asc()).paginate(page=page, per_page=per_page)
     terms_dicts = []
-    for term in terms:
+    for term in terms.items:
         terms_dicts.append(term_to_dict(term))
     return terms_dicts
 
